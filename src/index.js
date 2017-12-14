86291d95cbd5a6e210d7c1a5fa8a19b98418e350
dujinqiao <duzi1996@qq.com>
2017-12-14 10:04:26 +0800

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Desk from './components/Deskmark/index';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-
-
-ReactDom.render(<Desk/>, document.getElementById('root'));
